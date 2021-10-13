0d8966c10c8e972dfb550cc9660c3286cdd025ea
Nora8080 <89574722+Nora8080@users.noreply.github.com>
2021-10-13 14:19:02 +0300

    done

---
+++ HW2/HW2/HW2/HW2(if-statement).swift
@@ -0,0 +1,54 @@
+//
+//  HW2(if-statement).swift
+//  HW2(if-statement)
+//
+//  Created by نورا on 07/03/1443 AH.
+//
+
+import Foundation
+
+//print("Enter the magnitude", terminator: "")
+//print()
+//var magnitude = Utils.readDouble()
+//
+//if magnitude < 2.0 {
+//    print("earthquake is micro")
+//}
+//
+//else if magnitude == 2.0 || magnitude < 3.0 {
+//    print("earthquake is very minor")
+//}
+//
+//else if magnitude == 3.0 || magnitude < 4.0 {
+//    print("earthquake is minor")
+//}
+//
+//else if magnitude == 4.0 || magnitude < 5.0 {
+//    print("earthquake  is light")
+//}
+//
+//else if magnitude == 5.0 || magnitude < 6.0 {
+//    print("earthquake is moderate")
+//}
+//
+//else if magnitude == 6.0 || magnitude < 7.0 {
+//    print("earthquake is Strong")
+//}
+//
+//else if magnitude == 7.0 || magnitude < 8.0 {
+//    print("earthquake is Major")
+//}
+//
+//else if magnitude == 8.0 || magnitude < 10.0 {
+//    print("earthquake is Great")
+//}
+//
+//else if magnitude >= 10.0 {
+//    print("earthquake is Meteoric")
+//}
+//else {
+//    print("There is no earthquake")
+//}
+//
+//
+
---
+++ HW2/HW2/HW2/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  HW2
+//
+//  Created by نورا on 07/03/1443 AH.
+//
+
+import Foundation
+
